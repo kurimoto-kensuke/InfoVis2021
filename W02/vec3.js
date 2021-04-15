e04d133c19d720d113ca080a67022040cec44848
kurimoto-kensuke <82433009+kurimoto-kensuke@users.noreply.github.com>
2021-04-15 15:23:39 +0900

    Initial commit

---
+++ W02/vec3.js
@@ -0,0 +1,88 @@
+class Vec3
+{
+
+
+    // Constructor
+    constructor( x , y , z )
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    add( v )
+    {
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
+        return this;
+    }
+
+    sum()
+    {
+        return this.x + this.y + this.z;
+
+    }
+
+    sub( v )
+    {
+        this.x -= v.x;
+        this.y -= v.y;
+        this.z -= v.z;
+        return this;
+    }
+
+
+    min()
+    {
+        let m;
+        if(this.x<this.y){
+            m = this.x;
+        }else{
+            m = this.y;
+        }
+
+        if(this.z<m){
+            return this.z;
+        }else{
+            return m;
+        }
+
+    }
+
+    mid()
+    {
+        return this.sum() - this.min() - this.max();
+    }
+
+    max()
+    {
+        let m;
+        if(this.x<this.y){
+             m = this.y;
+        }else{
+             m = this.x;
+        }
+
+        if(this.z<m){
+            return m;
+        }else{
+            return this.z;
+        }
+    }
+
+    cross( v )
+    {
+        var x = this.x, y = this.y, z = this.z;
+        this.x = y * v.z - z * v.y;
+        this.y = z * v.x - x * v.z;
+        this.z = x * v.y - y * v.x;
+        return this;
+    }
+
+    length()
+    {
+        return Math.sqrt( this.x * this.x + this.y * this.y + this.z * this.z );
+    }
+
+}
